2ca789a675b724f73278bdd7909e6e3a7cc39d92
Pengyao <pengyao.cui@cern.ch>
2020-10-09 18:14:23 +0800

    Add some results plots

--- run.sh
+++ run.sh
@@ -4,29 +4,41 @@ root -l -b -q f1_1.C
 root -l -b -q f1_2.C
 root -l -b -q f1_3.C
 root -l -b -q f1_4.C
+
 root -l -b -q f2_1.C
 root -l -b -q f2_2.C
 root -l -b -q f3_1.C
 root -l -b -q f3_2.C
+
 root -l -b -q f4_1.C
 root -l -b -q f4_2.C
 root -l -b -q f4_3.C
 root -l -b -q f4_4.C
+
 root -l -b -q f5_1.C
 root -l -b -q f5_2.C
 root -l -b -q f5_3.C
 root -l -b -q f5_4.C
+
 root -l -b -q f6_1.C
 root -l -b -q f6_2.C
 root -l -b -q f6_3.C
 root -l -b -q f6_4.C
 root -l -b -q f6_5.C
 root -l -b -q f6_6.C
+
 root -l -b -q f7_1.C
 root -l -b -q f7_2.C
 root -l -b -q f7_3.C
+root -l -b -q f7_4.C
+root -l -b -q f7_5.C
+root -l -b -q f7_6.C
+
 root -l -b -q f8_1.C
 root -l -b -q f8_2.C
 root -l -b -q f8_3.C
+root -l -b -q f8_4.C
+root -l -b -q f8_5.C
+root -l -b -q f8_6.C
 
 cp ./figure/pdf/* /home/cuipengyao/Desktop/Writting/paper/figures/results
